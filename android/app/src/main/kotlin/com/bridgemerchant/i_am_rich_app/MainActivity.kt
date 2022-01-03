5fced6607acaea28b5c85cd6e0aa0493ab4b3010
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-01-03 04:30:35 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/bridgemerchant/i_am_rich_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bridgemerchant.i_am_rich_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//the main function is the starting point for all our Flutter apps
+void main() {
+  runApp(const MaterialApp(
+      home: Center(
+    child: Text("Hello world"),
+  )));
+}
