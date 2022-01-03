022b0f7627467abae20125b11da5ed3535b028a6
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-01-03 09:42:12 +0100

    added the app bar to the app and also changed the background color of the scaffold content area

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 
 //the main function is the starting point for all our Flutter apps
 void main() {
-  runApp(const MaterialApp(
-      home: Center(
-    child: Text("Hello world"),
-  )));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey.shade200,
+        appBar: AppBar(
+          title: const Text(
+            "I Am Rich",
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Content,
+      ),
+    ),
+  );
 }
